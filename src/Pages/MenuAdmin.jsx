06a143aa98cfe924581b8332064a91dbdfe3213d
Brayan M <brihanm.work@gmail.com>
2024-10-01 18:04:50 -0500

    Se agreggó Css al diseño de MenuAdmin

--- src/Pages/MenuAdmin.jsx
+++ src/Pages/MenuAdmin.jsx
@@ -4,35 +4,35 @@ const MenuAdmin = () => {
   return (
     <div>
       <div class="container">
-        <h1>Administrador de Alquiler de Autos</h1>
+        <h1 class="tittle">Administrador de Alquiler de Autos</h1>
         <section id="add-car">
-            <h2>Agregar Nuevo Auto</h2>
+            <h2 class="tittle">Agregar Nuevo Auto</h2>
             <form id="add-car-form">
                 <div class="form-group">
                     <label for="car-make">Marca:</label>
                     <input type="text" id="car-make" name="car-make" required/>
                 </div>
                 <div class="form-group">
-                    <label for="car-model">Modelo:</label>
+                    <label for="car-model" class="labels" >Modelo:</label>
                     <input type="text" id="car-model" name="car-model" required/>
                 </div>
                 <div class="form-group">
-                    <label for="car-year">Año:</label>
+                    <label for="car-year" class="labels">Año:</label>
                     <input type="number" id="car-year" name="car-year" required/>
                 </div>
                 <div class="form-group">
-                    <label for="car-price">Precio por Día:</label>
+                    <label for="car-price" class="labels">Precio por Día:</label>
                     <input type="number" id="car-price" name="car-price" required/>
                 </div>
                 <div class="form-group">
-                    <label for="car-image">Imagen:</label>
+                    <label for="car-image" class="labels">Imagen:</label>
                     <input type="file" id="car-image" name="car-image" required/>
                 </div>
-                <button type="submit">Agregar Auto</button>
+                <button type="submit" class="button-add">Agregar Auto</button>
             </form>
         </section>
         <section id="rented-cars">
-            <h2>Autos rentados actualmente</h2>
+            <h2 class="tittle">Autos rentados actualmente</h2>
             <ul id="car-list">
             </ul>
         </section>
